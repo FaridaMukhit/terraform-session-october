e765166073a690d5b042383af4e79e6abd2e5e86
FaridaMukhit <mukhitfarida@gmail.com>
2022-11-02 00:43:04 +0000

    added

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,8 +1,11 @@
 module "ec2" {
-  source        = "github.com/FaridaMukhit/terraform-session-october//modules/ec2"
+  source        = "github.com/FaridaMukhit/terraform-session-october//modules/ec2?ref=v1.0.0"
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
   env           = "dev"
   s3_bucket_name = "test"
-
+}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
 }
